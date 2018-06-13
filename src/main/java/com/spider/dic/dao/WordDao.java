fbba4c79bbcdad12d518882354c206734a876090
wangjianjie606@gmail.com <wangjianjie606@gmail.com>
2018-06-13 21:07:56 +0800

    add search page

--- src/main/java/com/spider/dic/dao/WordDao.java
+++ src/main/java/com/spider/dic/dao/WordDao.java
@@ -3,8 +3,13 @@ package com.spider.dic.dao;
 import com.spider.dic.entity.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WordDao extends JpaRepository<Word, String> {
+
     Word findWordByWordEquals(String word);
 
     boolean existsByWord(String word);
+
+//    List<Word> f
 }
